23183c254219c33cb822ad3d5814bb99b9283cfa
Artem Vysotsky <artem.vysotsky@gmail.com>
2016-05-12 15:51:43 -0700

    Added observer data structure

---
+++ common/observer/observer.go
@@ -0,0 +1,45 @@
+package observer
+
+import "sync"
+
+//Observer interface
+type Observer interface {
+	Publish(event string, data interface{}) error
+	Subscribe(event string, eventListener EventListener) error
+}
+
+//EventListener is callback function
+type EventListener func(data interface{}) error
+
+//ObserverImpl implements Observer interface
+type ObserverImpl struct {
+	mutex     sync.Mutex
+	listeners map[string][]EventListener
+}
+
+//New instantiates ObserverImpl
+func New() *ObserverImpl {
+	return &ObserverImpl{
+		listeners: make(map[string][]EventListener, 0),
+	}
+}
+
+//Publish implements Observer interface
+func (o *ObserverImpl) Publish(event string, data interface{}) error {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+	for _, listener := range o.listeners[event] {
+		if err := listener(data); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//Subscribe implements Observer interface
+func (o *ObserverImpl) Subscribe(event string, eventListener EventListener) {
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+	o.listeners[event] = append(o.listeners[event], eventListener)
+}
