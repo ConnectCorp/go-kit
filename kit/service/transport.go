40644bdc21096367a797a9126fb30001735edfa9
Ivan Bertona <ivan.bertona@gmail.com>
2016-06-22 16:17:33 -0700

    Add JSONDecoderMixin.

--- kit/service/transport.go
+++ kit/service/transport.go
@@ -46,22 +46,6 @@ type ErrorResponse struct {
 	Error string `json:"error,omitempty"`
 }
 
-// MakePOSTRequestDecoder generates a go-kit request decoder that parses the request body as JSON.
-func MakePOSTRequestDecoder(requestType reflect.Type) kithttp.DecodeRequestFunc {
-	if requestType.Kind() != reflect.Struct {
-		panic(xerror.New("requestType must have kind = struct.", requestType))
-	}
-	return func(ctx context.Context, r *http.Request) (interface{}, error) {
-		req := reflect.New(requestType).Interface()
-		err := json.NewDecoder(r.Body).Decode(req)
-		defer r.Body.Close()
-		if err != nil {
-			return nil, xerror.Wrap(xerror.Wrap(err, ErrorCannotDecode, r), ErrorBadRequest)
-		}
-		return req, nil
-	}
-}
-
 // ErrorToStatusCode converts an error to the corresponding HTTP status code.
 func ErrorToStatusCode(err error) int {
 	if xerror.Is(err, ErrorBadRequest) {
@@ -203,4 +187,20 @@ func (*JSONErrorEncoderMixin) ErrorEncoder(_ context.Context, err error, w http.
 	w.Header().Add(defaultContentTypeHeaderName, defaultContentTypeHeaderValue)
 	w.WriteHeader(ErrorToStatusCode(err))
 	_ = json.NewEncoder(w).Encode(&ErrorResponse{Error: err.Error()}) // Ignores an encoding error.
+}
+
+// JSONDecoderMixin is a mixin implementing part of the Route interface.
+type JSONDecoderMixin struct {
+	RequestType reflect.Type
+}
+
+// Decoder implements the Route interface.
+func (d *JSONDecoderMixin) Decoder(ctx context.Context, r *http.Request) (interface{}, error) {
+	req := reflect.New(d.RequestType).Interface()
+	err := json.NewDecoder(r.Body).Decode(req)
+	defer r.Body.Close()
+	if err != nil {
+		return nil, xerror.Wrap(xerror.Wrap(err, ErrorCannotDecode, r), ErrorBadRequest)
+	}
+	return req, nil
 }
