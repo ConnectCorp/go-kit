bd0bed6bd8455974cd69590b2528c0b63b5f23b3
Ivan Bertona <ivan.bertona@gmail.com>
2016-08-12 14:26:24 -0700

    Add MD5 util function.

---
+++ kit/utils/crypto.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"crypto/md5"
+	"io"
+	"fmt"
+)
+
+
+// SaltAndHash returns a SHA-256 hash of the salted subject, formatted as hex string.
+func SaltAndHash(subject, salt string) string {
+	h := sha256.New()
+	io.WriteString(h, subject+salt)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// MD5 returns the MD5 hash of the given string.
+func MD5(subject string) string {
+	h := md5.New()
+	io.WriteString(h, subject)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
--- kit/utils/random_test.go
+++ kit/utils/random_test.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"regexp"
 	"testing"
@@ -27,14 +26,5 @@ func TestGenRandomIntRange(t *testing.T) {
 	for i := 0; i < 10000; i++ {
 		v := GenRandomIntRange(100, 1000)
 		assert.True(t, v >= 100 && v <= 1000)
-	}
-}
-
-func TestSaltAndHash(t *testing.T) {
-	r := regexp.MustCompile("^[0-9a-f]+$")
-	for i := 0; i < 1000; i++ {
-		v := SaltAndHash("subject", fmt.Sprintf("%v", i))
-		assert.Equal(t, 64, len(v))
-		assert.True(t, r.MatchString(v))
 	}
 }
