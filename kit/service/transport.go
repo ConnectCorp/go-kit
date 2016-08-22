80186ca9d1d671bab7495b769a7c9197be83adff
Stephen <stephen.sunday@andela.com>
2016-08-22 19:24:06 +0100

    Handle CORS

--- kit/service/transport.go
+++ kit/service/transport.go
@@ -98,6 +98,19 @@ func NewRouter(
 		w.Write([]byte("ok"))
 	})
 
+	//handle CORS
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+	})
+
+
 	return &Router{
 		rootCtx: context.Background(),
 		//metricsReporter: NewMetricsReporter(commonMetricsNamespace, svcName, dogstatsdEmitter),
