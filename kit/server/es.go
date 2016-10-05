7cfa114022a5f3cfaa57d776b676ab7bd7bc2927
Ivan Bertona <ivan.bertona@gmail.com>
2016-10-05 16:03:08 -0700

    Add MustInitES helper.

---
+++ kit/server/es.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"github.com/ConnectCorp/go-kit/kit/utils"
+	"gopkg.in/olivere/elastic.v2"
+	"net/url"
+	"time"
+)
+
+const (
+	baseESInitDelay     = 25 * time.Millisecond
+	maxESInitRetryCount = 10
+	defaultESMaxRetries = 5
+)
+
+// MustInitES initializes an ElasticSearch client, or panics.
+func MustInitES(esSpec *url.URL) elastic.Client {
+	var es elastic.Client
+	var err error
+
+	utils.MustBackoff(baseESInitDelay, maxESInitRetryCount, func() error {
+		es, err = elastic.NewClient(elastic.SetURL(esSpec), elastic.SetMaxRetries(defaultESMaxRetries))
+		return err
+	})
+
+	return es
+}
