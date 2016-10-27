e3da3a476f8fa579f7c840ef2e0253947ed8e56f
Ivan Bertona <ivan.bertona@gmail.com>
2016-10-26 17:30:15 -0700

    Use regular neo4j driver until pooling driver is fixed.

--- kit/server/neo4j.go
+++ kit/server/neo4j.go
@@ -1,37 +1,25 @@
 package server
 
 import (
-	"github.com/ConnectCorp/go-kit/kit/utils"
 	neo4j "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 	"net/url"
-	"time"
 )
 
-const (
-	baseNeo4JInitDelay         = 25 * time.Millisecond
-	maxNeo4JInitRetryCount     = 10
-	defaultNeo4JDriverPoolSize = 10
-)
+// Neo4JConnProvider is a connection provider for a neo4j database.
+type Neo4JConnProvider struct {
+	spec   string
+	driver neo4j.Driver
+}
+
+// GetConn returns a new connection to neo4j.
+func (n *Neo4JConnProvider) GetConn() (neo4j.Conn, error) {
+	return n.driver.OpenNeo(n.spec)
+}
 
 // MustInitNeo4J initializes a Neo4J driver pool, or panics.
-func MustInitNeo4J(spec *url.URL) neo4j.DriverPool {
-	var driverPool neo4j.DriverPool
-	var err error
-
-	utils.MustBackoff(baseNeo4JInitDelay, maxNeo4JInitRetryCount, func() error {
-		driverPool, err = neo4j.NewDriverPool(spec.String(), defaultNeo4JDriverPoolSize)
-		if err != nil {
-			return err
-		}
-
-		conn, err := driverPool.OpenPool()
-		if err != nil {
-			return err
-		}
-		defer conn.Close()
-
-		return nil
-	})
-
-	return driverPool
+func MustInitNeo4J(spec *url.URL) *Neo4JConnProvider {
+	return &Neo4JConnProvider{
+		spec:   spec.String(),
+		driver: neo4j.NewDriver(),
+	}
 }
