17279ad53feacc4b9421c6faa0088adcb40f55f2
Ivan Bertona <ivan.bertona@gmail.com>
2016-10-28 10:45:33 -0700

    Wait for neo4j connectivity.

--- kit/server/neo4j.go
+++ kit/server/neo4j.go
@@ -1,8 +1,15 @@
 package server
 
 import (
+	"github.com/ConnectCorp/go-kit/kit/utils"
 	neo4j "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 	"net/url"
+	"time"
+)
+
+const (
+	baseNeo4JInitDelay     = 25 * time.Millisecond
+	maxNeo4JInitRetryCount = 10
 )
 
 // Neo4JConnProvider is a connection provider for a neo4j database.
@@ -18,8 +25,19 @@ func (n *Neo4JConnProvider) GetConn() (neo4j.Conn, error) {
 
 // MustInitNeo4J initializes a Neo4J driver pool, or panics.
 func MustInitNeo4J(spec *url.URL) *Neo4JConnProvider {
-	return &Neo4JConnProvider{
+	p := &Neo4JConnProvider{
 		spec:   spec.String(),
 		driver: neo4j.NewDriver(),
 	}
+
+	utils.MustBackoff(baseNeo4JInitDelay, maxNeo4JInitRetryCount, func() error {
+		conn, err := p.GetConn()
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+		return nil
+	})
+
+	return p
 }
