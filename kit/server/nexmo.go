d49836ff32972674e246ff43503361dd6b94b24b
Artem Vysotsky <artem.vysotsky@gmail.com>
2016-11-09 16:56:31 -0800

    Update nexmo.go

--- kit/server/nexmo.go
+++ kit/server/nexmo.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/PuerkitoBio/rehttp"
 	"github.com/njern/gonexmo"
-	"io/ioutil"
 )
 
 // MustInitNexmo initializes the Nexmo client, or panics.
@@ -20,28 +19,9 @@ func MustInitNexmo(commonCfg *CommonConfig, nexmoCfg *NexmoConfig) *nexmo.Client
 			rehttp.RetryAny(
 				rehttp.RetryTemporaryErr(),
 				rehttp.RetryStatusInterval(500, 600),
-				nexmoThrottleRetry,
 			),
 		),
 	)
 
 	return nexmoClient
-}
-
-func nexmoThrottleRetry(attempt rehttp.Attempt) bool {
-	bytes, err := ioutil.ReadAll(attempt.Response.Body)
-	if err == nil {
-		return false
-	}
-
-	var resp struct {
-		Status string `json:"status"`
-	}
-
-	err = json.Unmarshal(bytes, resp)
-	if err == nil {
-		return false
-	}
-
-	return resp.Status == "1"
 }
